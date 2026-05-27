325095d4390317b6f77649bed6a4d267fe7f4ebe
Lucianno Lopes <lucislopes@gmail.com>
2026-05-26 23:05:41 -0300

    rota temporaria para add admin@sistema.com

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 
+import bcrypt from "bcrypt"
+import { prisma } from "./lib/prisma.js"
+
 import authRoutes from "./routes/auth.routes.js"
 import clientesRoutes from "./routes/clientes.routes.js"
 import funcionariosRoutes from "./routes/funcionarios.routes.js"
@@ -25,7 +28,6 @@ import configuracaoEmpresaRoutes from "./routes/configuracaoEmpresa.routes.js"
 
 
 dotenv.config()
-
 const app = express()
 
 app.use(cors())
@@ -59,4 +61,34 @@ app.get("/", (req, res) => {
 
 app.listen(3333, () => {
   console.log("Servidor rodando na porta 3333")
+})
+
+
+
+app.get("/criar-admin", async (req, res) => {
+  try {
+
+    const senhaHash = await bcrypt.hash("123456", 10)
+
+    const usuario = await prisma.usuario.create({
+      data: {
+        email: "admin@sistema.com",
+        senha: senhaHash,
+
+        funcionario: {
+          create: {
+            nome: "Administrador",
+            ativo: true,
+            funcao: "ADMIN"
+          }
+        }
+      }
+    })
+
+    res.json(usuario)
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
 })
